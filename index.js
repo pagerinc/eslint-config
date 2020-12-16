d515af7954454584e5f7f68010769a6bbbeb84f1
Diego Rodríguez Baquero <diegorbaquero@gmail.com>
2020-12-16 11:55:24 -0500

    feat: no-await-in-loop

--- index.js
+++ index.js
@@ -11,14 +11,15 @@ module.exports = {
         ecmaVersion: 2020
     },
     rules: {
+        'no-await-in-loop': 'error',
         'no-console': 'error',
+        'require-await': 'off',
         'import/order': [
             'error',
             {
                 'groups': ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
                 'newlines-between': 'always'
             }
-        ],
-        'require-await': 'off'
+        ]
     }
 };
